df5594c8e2590cd1c618c563353c795573772d47
Noah Cain <88724898+NCain24@users.noreply.github.com>
2023-02-16 15:09:50 -0800

    updated styling on skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,71 +11,63 @@ const Skills = () => {
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center p-8">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
               alt="React icon"
-              className="w-20 mx-auto"
+              className="w-28 mx-auto"
             />
-            <p className="my-4">ReactJS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
               alt="Mongo icon"
-              className="w-20 mx-auto"
+              className="w-28 mx-auto"
             />
-            <p className="my-4">MongoDB</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
               alt="Express icon"
-              className="w-20 mx-auto"
+              className="w-28 mx-auto"
             />
-            <p className="my-4">ExpressJS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg"
               alt="Node icon"
-              className="w-20 mx-auto"
+              className="w-28 mx-auto"
             />
-            <p className="my-4">NodeJS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
               alt="tailwind icon"
-              className="w-20 mx-auto"
+              className="w-28  mx-auto"
             />
-            <p className="my-4">Tailwind CSS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
               alt="JavaScript icon"
-              className="w-20 mx-auto"
+              className="w-28  mx-auto"
             />
-            <p className="my-4">JavaScript</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
               alt="Postgres icon"
-              className="w-20 mx-auto"
+              className="w-28 p- mx-auto"
             />
-            <p className="my-4">PostgreSQL</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg">
+          <div className="shadow-md shadow-[#040c16] bg-gray-400 text-black rounded-lg p-2">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
               alt="HTML5 icon"
-              className="w-20 mx-auto"
+              className="w-28 pt-2 mx-auto"
             />
 
-            <p className="my-4">HTML5</p>
           </div>
         </div>
       </div>
